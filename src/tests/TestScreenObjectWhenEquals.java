43376bde9869671ab9e949e8b32b035c8eebbd14
frankminyon <frankminyon@192.168.58.101>
2017-10-03 03:37:10 -0400

    ScreenObject::equals() tested

---
+++ src/tests/TestScreenObjectWhenEquals.java
@@ -0,0 +1,70 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import model.ScreenObject;
+
+public class TestScreenObjectWhenEquals {
+	
+	/**
+	 * Testing with 0 parameter constructor
+	 */
+	@Test
+	public void testDefaultTrue() {
+		ScreenObject item = new ScreenObject();
+		ScreenObject otherItem = new ScreenObject();
+		assertEquals(true, item.equals(otherItem));
+	}
+
+	/**
+	 * Testing with 3 parameter constructor
+	 * 	all values equal eachother
+	 * 	true
+	 */
+	@Test
+	public void test3ParamTrue() {
+		ScreenObject item = new ScreenObject(25, 25, true);
+		ScreenObject otherItem = new ScreenObject(25, 25, true);
+		assertEquals(true, item.equals(otherItem));
+	}
+
+	/**
+	 * Testing with 3 parameter constructor
+	 * 	different x coordinate
+	 * 	false
+	 */
+	@Test
+	public void testDifferentXFalse() {
+		ScreenObject item = new ScreenObject(25, 25, true);
+		ScreenObject otherItem = new ScreenObject(20, 25, true);
+		assertEquals(false, item.equals(otherItem));
+	}
+
+	/**
+	 * Testing with 3 parameter constructor
+	 * 	different y coordinate
+	 * 	false
+	 */
+	@Test
+	public void testDifferentYFalse() {
+		ScreenObject item = new ScreenObject(25, 25, true);
+		ScreenObject otherItem = new ScreenObject(25, 20, true);
+		assertEquals(false, item.equals(otherItem));
+	}
+
+	/**
+	 * Testing with 3 parameter constructor
+	 * 	different visibility
+	 * 	false
+	 */
+	@Test
+	public void testDifferentVisibilityFalse() {
+		ScreenObject item = new ScreenObject(25, 25, true);
+		ScreenObject otherItem = new ScreenObject(25, 25, false);
+		assertEquals(false, item.equals(otherItem));
+	}
+	
+}
