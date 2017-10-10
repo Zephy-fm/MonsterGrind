4b0db60da1dfe47b0fed013a4fb05f45a340e6e0
FrankMinyon <FrankMinyon@192.168.58.57>
2017-10-10 18:29:17 -0400

    Player added

---
+++ java/model/Player.java
@@ -0,0 +1,26 @@
+package model;
+
+import java.util.Map;
+
+public class Player {
+	private String name;
+	private int hp;
+	private int mp;
+	private int strength;
+	private int dexterity;
+	private int intellect;
+	private int defense;
+	private int luck;
+	private int level;
+	private int experience;
+	private int experienceToLevelUp;
+	private Equipment weapon;
+	private Equipment armor;
+	private Equipment accessory1;
+	private Equipment accessory2;
+
+	public Player(Map<String, String> playerMap) {
+		
+	}
+	
+}
