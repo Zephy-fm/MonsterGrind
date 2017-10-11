3a0c1ddfd31b563f9ef79e6c43864ae1672835e6
frankminyon <frankminyon@192.168.58.101>
2017-10-11 00:54:30 -0400

    Equipment added

---
+++ java/model/Equipment.java
@@ -0,0 +1,21 @@
+package model;
+
+import java.util.Map;
+
+public abstract class Equipment {
+	private String name;
+	private String description;
+	private int hp;
+	private int mp;
+	private int strength;
+	private int dexterity;
+	private int intellect;
+	private int defense;
+	private int luck;
+	private String special;
+	
+	public Equipment(Map<String, String> equipmentMap) {
+		
+	}
+
+}
